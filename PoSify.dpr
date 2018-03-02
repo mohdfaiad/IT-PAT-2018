751bd5e9d3260c2934a6b27da6086b6a85111b02
Stephan Cilliers <stephanus.cilliers@gmail.com>
2018-03-02 13:04:01 +0200

    Update changes to project

--- PoSify.dpr
+++ PoSify.dpr
@@ -4,7 +4,8 @@ uses
   Forms,
   frmTemplate_U in 'frmTemplate_U.pas' {frmTemplate},
   TUser_U in 'TUser_U.pas',
-  TForm_Employee_Home in 'TForm_Employee_Home.pas' {frmEmployeeHome};
+  TForm_Employee_Home in 'TForm_Employee_Home.pas' {frmEmployeeHome},
+  Data_Module_U in 'Data_Module_U.pas' {data_module: TDataModule};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := False;
   Application.CreateForm(TfrmEmployeeHome, frmEmployeeHome);
+  Application.CreateForm(Tdata_module, data_module);
   Application.Run;
 end.
