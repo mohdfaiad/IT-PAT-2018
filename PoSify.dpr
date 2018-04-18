9b0fa8750f4811a416dff8769c7dc22eddbfcc19
Stephan Cilliers <stephanus.cilliers@gmail.com>
2018-04-18 16:43:10 +0200

    Saved

--- PoSify.dpr
+++ PoSify.dpr
@@ -7,14 +7,15 @@ uses
   TForm_Employee_Home in 'TForm_Employee_Home.pas' {frmEmployeeHome},
   Data_Module_U in 'Data_Module_U.pas' {data_module: TDataModule},
   TItem_U in 'TItem_U.pas',
-  TOrder_U in 'TOrder_U.pas';
+  TOrder_U in 'TOrder_U.pas',
+  Logger_U in 'Logger_U.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := False;
-  Application.CreateForm(TfrmEmployeeHome, frmEmployeeHome);
   Application.CreateForm(Tdata_module, data_module);
+  Application.CreateForm(TfrmEmployeeHome, frmEmployeeHome);
   Application.Run;
 end.
