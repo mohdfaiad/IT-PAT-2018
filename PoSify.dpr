062ee8829235b19210795185a02ffd6cf26f6dda
Stephan Cilliers <stephanus.cilliers@gmail.com>
2018-04-23 13:59:30 +0200

    Saved

--- PoSify.dpr
+++ PoSify.dpr
@@ -8,7 +8,8 @@ uses
   Data_Module_U in 'Data_Module_U.pas' {data_module: TDataModule},
   TItem_U in 'TItem_U.pas',
   TOrder_U in 'TOrder_U.pas',
-  Logger_U in 'Logger_U.pas';
+  Logger_U in 'Logger_U.pas',
+  Utilities_U in 'Utilities_U.pas';
 
 {$R *.res}
 
